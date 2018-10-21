3e4f9f626581567abba11354e5fb1cc9d3dd0489
kristianzivkovic357 <kristiano9611@gmail.com>
2018-10-21 12:35:18 +0200

    Removed CORS headers.

--- app.js
+++ app.js
@@ -11,12 +11,6 @@ const { PORT } = process.env;
 
 const app = express();
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
-
 app.use('/', routes);
 app.disable('x-powered-by');
 
