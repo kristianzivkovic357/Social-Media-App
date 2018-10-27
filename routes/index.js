8b73cb011c3cd6f4aa36fd4d6cf62ca29539a1cb
Jovan <jjovanovic24@hotmail.com>
2018-10-27 15:17:51 +0200

    FIix route

--- routes/index.js
+++ routes/index.js
@@ -22,7 +22,7 @@ router.post(`${baseUrl}/users/register`, val.users.register, userRoutes.register
 router.post(`${baseUrl}/users/login`, val.users.login, userRoutes.login);
 router.post(`${baseUrl}/users/logout`, userRoutes.logout);
 
-router.get(`${baseUrl}/users/register_access_token`, authc.service, authc.user, val.users.accessKey, userRoutes.accessToken);
+router.get(`${baseUrl}/users/register_access_token/:network`, authc.service, authc.user, val.users.accessKey, userRoutes.accessToken);
 router.post(`${baseUrl}/users/:id/register_access_token`, authc.service, authc.user, val.users.accessKey, userRoutes.accessToken);
 
 router.get(`${baseUrl}/users/:id/posts`, authc.service, authc.user, val.users.getPosts, userRoutes.getPosts);
